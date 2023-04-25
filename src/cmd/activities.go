89c6d381b18fb19d004471023b1675ba60499e3c
Seán C McCord <ulexus@gmail.com>
2023-04-25 14:46:18 -0400

    lint fixes

--- src/cmd/activities.go
+++ src/cmd/activities.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"github.com/pkg/errors"
 	"github.com/tkhq/go-sdk/pkg/api/client/activities"
 	"github.com/tkhq/go-sdk/pkg/api/models"
 
@@ -16,10 +15,11 @@ func init() {
 	rootCmd.AddCommand(activitiesCmd)
 
 	activitiesCmd.AddCommand(activitiesListCmd)
+	activitiesCmd.AddCommand(activitiesGetCmd)
 }
 
 var activitiesCmd = &cobra.Command{
-	Use:   "activities interacts with the API activities",
+	Use:   "activities",
 	Short: "activities interacts with the API activities",
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
 		LoadKeypair("")
@@ -28,17 +28,25 @@ var activitiesCmd = &cobra.Command{
 }
 
 var activitiesListCmd = &cobra.Command{
-	Use:   "list returns the set of activities for an organization",
+	Use:   "list",
 	Short: "list returns the set of activities for an organization",
 	Run: func(cmd *cobra.Command, args []string) {
 		activitiesFilter := make([]models.V1ActivityStatus, len(activitiesListStatus))
 
 		for i, s := range activitiesListStatus {
-			switch s {
-			case "all":
+			if s == Help {
+				Output(models.ActivityStatuses())
+
+				return
+			}
+
+			if s == "all" {
 				activitiesFilter = models.ActivityStatuses()
 
 				break
+			}
+
+			switch s {
 			case "created":
 				activitiesFilter[i] = models.V1ActivityStatusACTIVITYSTATUSCREATED
 			case "pending":
@@ -68,10 +76,27 @@ var activitiesListCmd = &cobra.Command{
 			OutputError(err)
 		}
 
-		if res.Error() != "" {
-			OutputError(errors.Errorf("request failed: %s", res.Error()))
+		Output(res.GetPayload().Activities)
+	},
+}
+
+var activitiesGetCmd = &cobra.Command{
+	Use:   "get <activity-id>",
+	Short: "get returns the details and status of a particular activity",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		id := args[0]
+
+		params := activities.NewPublicAPIServiceGetActivityParams().WithDefaults().WithBody(&models.V1GetActivityRequest{
+			ActivityID:     &id,
+			OrganizationID: &Organization,
+		})
+
+		res, err := APIClient.V0().Activities.PublicAPIServiceGetActivity(params, APIClient.Authenticator)
+		if err != nil {
+			OutputError(err)
 		}
 
-		Output(res.GetPayload().Activities)
+		Output(res.GetPayload().Activity)
 	},
 }
