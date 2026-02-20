26236e4e1e1a0383670939e666a94800b766a359
Ben Wilson <ben@turnkey.io>
2026-02-20 14:02:51 -0500

    fix: flake go version bump for workspaces

--- flake.nix
+++ flake.nix
@@ -2,14 +2,18 @@
   description = "tkcli devshell";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs-go.url = "github:nixos/nixpkgs/nixos-23.11";
     #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # localstack is broken right now (2023-01-25) in unstable, due to a missing dependency
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+  outputs = { self, nixpkgs, nixpkgs-go, flake-utils, ... }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        goPkgs = nixpkgs-go.legacyPackages.${system};
+        go = goPkgs.go_1_21;
+        golangci-lint = goPkgs.golangci-lint;
 
         gci = pkgs.buildGoModule rec {
            name = "gci";
@@ -28,19 +32,19 @@
         tkbuild = pkgs.writeScriptBin "build" ''
           #!/bin/sh
           pushd $(git rev-parse --show-toplevel)/src
-          ${pkgs.go}/bin/go build -o $(go env GOPATH)/bin/turnkey
-          ${pkgs.go}/bin/go build -o ../out/turnkey.linux-x86_64 # hack for local CLI go test
+          ${go}/bin/go build -o $(go env GOPATH)/bin/turnkey ./cmd/turnkey
+          ${go}/bin/go build -o ../out/turnkey.linux-x86_64 ./cmd/turnkey # hack for local CLI go test
         '';
 
         tklint = pkgs.writeScriptBin "lint" ''
           #!/bin/sh
           pushd $(git rev-parse --show-toplevel)/src
-          ${pkgs.go}/bin/go mod tidy
-          ${pkgs.gofumpt}/bin/gofumpt -w *.go ./cmd/*
+          ${go}/bin/go mod tidy
+          ${pkgs.gofumpt}/bin/gofumpt -w ./cmd/*
           ${gci}/bin/gci write --skip-generated -s standard -s default -s "Prefix(github.com/tkhq)" .
-          ${pkgs.golangci-lint}/bin/golangci-lint run ./...
-          ${pkgs.go}/bin/go build -o ../out/turnkey.linux-x86_64 # hack for local CLI go test
-          ${pkgs.go}/bin/go test -v ./...
+          ${golangci-lint}/bin/golangci-lint run ./...
+          ${go}/bin/go build -o ../out/turnkey.linux-x86_64 ./cmd/turnkey # hack for local CLI go test
+          ${go}/bin/go test -v ./...
         '';
       in
       {
